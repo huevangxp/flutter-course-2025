47b21a6968d39d4831b8ffed4d44a8dd3c4226ac
huevangxp <huevang933@gmail.com>
2025-11-12 09:51:13 +0700

    commit Wed 11/12/2025  9:51:13.18

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -103,7 +103,7 @@ class Introduc extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.center,
         mainAxisSize: MainAxisSize.min,
         children: [
-          Icon(Icons.info, size: textFontSize * 2),
+          Icon(Icons.info, size: textFontSize * 2, color: Colors.red,),
           Text(
             'ສະບາຍດີ',
             style: TextStyle(
