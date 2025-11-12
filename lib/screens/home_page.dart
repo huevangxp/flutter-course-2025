ba318ec124c43ba98244509578f8289dfd33fb68
huevangxp <huevang933@gmail.com>
2025-11-12 10:28:58 +0700

    commit Wed 11/12/2025 10:28:58.16

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -87,9 +87,12 @@ class _HomePageState extends State<HomePage> {
                         
                         items: list
                             .map(
-                              (item) => Container(
-                                child: Text(item.toString() , style: TextStyle(color: Colors.white),),
-                                color: Colors.teal,
+                              (item) => ClipRRect(
+                                borderRadius: BorderRadius.circular(10),
+                                child: Container(
+                                  child: Text(item.toString() , style: TextStyle(color: Colors.white),),
+                                  color: Colors.teal,
+                                ),
                               ),
                             )
                             .toList(),
