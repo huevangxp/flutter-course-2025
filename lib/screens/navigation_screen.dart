2ceb9cbc35946d27356189dd49188a21d8665895
huevangxp <huevang933@gmail.com>
2025-11-14 08:24:31 +0700

    commit Fri 11/14/2025  8:24:31.14

---
+++ lib/screens/navigation_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class BottomNavigationScreen extends StatefulWidget {
+  const BottomNavigationScreen({super.key});
+
+  @override
+  State<BottomNavigationScreen> createState() => _BottomNavigationScreenState();
+}
+
+class _BottomNavigationScreenState extends State<BottomNavigationScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: Text('Bottom Navigation Screen')),
+    );
+  }
+}
