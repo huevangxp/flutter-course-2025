693dd2a180cc60a71caeb197ac8c8a7c33cadfed
huevangxp <huevang933@gmail.com>
2025-11-16 17:33:18 +0700

    commit ອາທິດ 16/11/2025 17:33:18,17

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -11,14 +11,18 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Image.asset('assets/images/appIcon.png', fit: BoxFit.contain, height: 100),
-          Text('field input login and button'),
-          Text('forget pass and signup')
-        ],
+      body: Container(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+         
+          children: [
+            Image.asset('assets/images/appIcon.png', fit: BoxFit.contain, height: 100),
+            Text('field input login and button'),
+            Text('forget pass and signup')
+          ],
+        ),
       )
     );
   }
