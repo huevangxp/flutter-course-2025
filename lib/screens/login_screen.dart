15413929db868ff609c5e3185e579f6e41753ddd
huevangxp <huevang933@gmail.com>
2025-11-18 11:08:34 +0700

    commit Tue 11/18/2025 11:08:34.18

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -32,8 +32,7 @@ class _LoginScreenState extends State<LoginScreen> {
 
               child: Column(
                 mainAxisSize: MainAxisSize.min,
-               
-
+                children: [
                   // Phone Number
                   TextField(
                     keyboardType: TextInputType.phone,
