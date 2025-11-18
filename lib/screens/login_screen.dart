ec9db30425301447be7683969a024b3407c39fd0
huevangxp <huevang933@gmail.com>
2025-11-18 08:50:43 +0700

    commit Tue 11/18/2025  8:50:43.18

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -11,20 +11,22 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        margin: const EdgeInsets.all(16),
-        color: Colors.red,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset(
-              'assets/images/appIcon.png',
-              fit: BoxFit.contain,
-              height: 100,
-            ),
-            Text('field input login and button', textAlign: TextAlign.center),
-            Text('forget pass and signup', textAlign: TextAlign.center),
-          ],
+      body: Center(
+        child: Container(
+          margin: const EdgeInsets.all(16),
+          color: Colors.red,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'assets/images/appIcon.png',
+                fit: BoxFit.contain,
+                height: 100,
+              ),
+              Text('field input login and button', textAlign: TextAlign.center),
+              Text('forget pass and signup', textAlign: TextAlign.center),
+            ],
+          ),
         ),
       ),
     );
