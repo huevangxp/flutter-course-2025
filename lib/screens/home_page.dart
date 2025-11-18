957e5d6abf3bdfebe64e6296ba8ea9a6746ba933
huevangxp <huevang933@gmail.com>
2025-11-18 13:48:54 +0700

    commit Tue 11/18/2025 13:48:54.13

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -83,7 +83,7 @@ class _HomePageState extends State<HomePage> {
               const SizedBox(height: 16),
               Category(categoryList),
               const SizedBox(height: 16),
-              Container(height: screenHeight * 0.2, color: Colors.teal),
+              Container(height: double.infinity, color: Colors.teal),
             ],
           ),
         ),
