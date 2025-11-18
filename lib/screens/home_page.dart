6c50b4ce70e75ecdf209cd21127de1f47faa1313
huevangxp <huevang933@gmail.com>
2025-11-18 13:46:48 +0700

    commit Tue 11/18/2025 13:46:48.16

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -107,8 +107,8 @@ class _HomePageState extends State<HomePage> {
               },
               child: Container(
                 padding: const EdgeInsets.symmetric(
-                  horizontal: 16,
-                  vertical: 8,
+                  horizontal: 10,
+                  vertical: 6,
                 ),
                 decoration: BoxDecoration(
                   color: isSelected ? Colors.teal : Colors.white,
