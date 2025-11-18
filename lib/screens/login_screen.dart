b1a36bc306fe5a1817f8e2751091e6b97d39ddcd
huevangxp <huevang933@gmail.com>
2025-11-18 10:37:41 +0700

    commit Tue 11/18/2025 10:37:41.18

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -74,7 +74,7 @@ class _LoginScreenState extends State<LoginScreen> {
                       'ເຂົ້າລົງ',
                       style: TextStyle(
                         color: Colors.white,
-                        fontSize: 20,
+                        fontSize: 16,
                         fontWeight: FontWeight.bold,
                       ),
                     ),
