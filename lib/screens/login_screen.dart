d6e76226317e7dd1f808eced5d1394ae9776320a
huevangxp <huevang933@gmail.com>
2025-11-18 08:51:25 +0700

    commit Tue 11/18/2025  8:51:25.21

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -15,13 +15,22 @@ class _LoginScreenState extends State<LoginScreen> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Image.asset(
-              'assets/images/appIcon.png',
-              fit: BoxFit.contain,
-              height: 100,
+            Container(
+              child: Column(
+                children: [
+                  Image.asset(
+                    'assets/images/appIcon.png',
+                    fit: BoxFit.contain,
+                    height: 100,
+                  ),
+                  Text(
+                    'field input login and button',
+                    textAlign: TextAlign.center,
+                  ),
+                  Text('forget pass and signup', textAlign: TextAlign.center),
+                ],
+              ),
             ),
-            Text('field input login and button', textAlign: TextAlign.center),
-            Text('forget pass and signup', textAlign: TextAlign.center),
           ],
         ),
       ),
